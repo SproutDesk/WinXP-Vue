eccb560e24c14938552bb1290e7d6ade7e0da170
Rhys Botfield <rhys4995@live.co.uk>
2020-05-08 21:40:12 +0100

    Draggable windows

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,21 @@
 <template>
     <div class="h-screen flex flex-col">
         <div class="desktop h-full w-full overflow-hidden relative bg-cover">
-            <div class="absolute bg-blue-500 m-10 Window has-border flex flex-col">
-                <div class="Window__header row">Title</div>
-                <div class="Window__content p-5">BBBBBBBBB</div>
-            </div>
+            <AppWindow />
         </div>
-        <Taskbar />
+        <SystemTaskbar />
     </div>
 </template>
 
 <script>
-import Taskbar from '@/components/Taskbar.vue'
+import SystemTaskbar from '@/components/SystemTaskbar.vue'
+import AppWindow from '@/components/AppWindow.vue'
 
 export default {
     name: 'App',
     components: {
-        Taskbar,
+        SystemTaskbar,
+        AppWindow,
     },
 }
 </script>
@@ -25,16 +24,5 @@ export default {
 .desktop {
     background-image: url('assets/desktop_bg.jpg');
 }
-.Window {
 
-}
-.Window.has-border {
-    border: #292ddf solid 2px;
-    border-top: none;
-    border-top-left-radius: 7px;
-    border-top-right-radius: 7px;
-}
-.Window__header {
-    background: rgba(0, 0, 0, 0) linear-gradient(rgb(0, 88, 238) 0%, rgb(53, 147, 255) 4%, rgb(40, 142, 255) 6%, rgb(18, 125, 255) 8%, rgb(3, 111, 252) 10%, rgb(2, 98, 238) 14%, rgb(0, 87, 229) 20%, rgb(0, 84, 227) 24%, rgb(0, 85, 235) 56%, rgb(0, 91, 245) 66%, rgb(2, 106, 254) 76%, rgb(0, 98, 239) 86%, rgb(0, 82, 214) 92%, rgb(0, 64, 171) 94%, rgb(0, 48, 146) 100%) repeat scroll 0% 0%;
-}
 </style>
--- src/components/SystemTaskbar.vue
+++ src/components/SystemTaskbar.vue
@@ -5,8 +5,8 @@
             src="@/assets/start.png"
             alt="Start">
         </div>
-        <div class="flex overflow-hidden px-2 py-2 text-xs">Active Windows</div>
-        <div :class="`${$options.name}__tray flex ml-auto max-w-xs overflow-hidden text-xs py-2 justify-between`">
+        <div class="flex overflow-hidden py-1 text-xs">Active Windows</div>
+        <div :class="`${$options.name}__tray flex ml-auto max-w-xs overflow-hidden py-2 justify-between`">
             <div class="mx-2">Tray Items</div>
             <div class="mx-2">{{ time }}</div>
         </div>
@@ -15,7 +15,7 @@
 
 <script>
 export default {
-    name: 'Taskbar',
+    name: 'SystemTaskbar',
     data() {
         return {
             time: this.getTime(),
@@ -42,22 +42,26 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.Taskbar {
+.SystemTaskbar {
     $root: &;
     $height: 30px;
     height: $height;
     background: rgba(0, 0, 0, 0) linear-gradient(rgb(31, 47, 134) 0px, rgb(49, 101, 196) 3%, rgb(54, 130, 229) 6%, rgb(68, 144, 230) 10%, rgb(56, 131, 229) 12%, rgb(43, 113, 224) 15%, rgb(38, 99, 218) 18%, rgb(35, 91, 214) 20%, rgb(34, 88, 213) 23%, rgb(33, 87, 214) 38%, rgb(36, 93, 219) 54%, rgb(37, 98, 223) 86%, rgb(36, 95, 220) 89%, rgb(33, 88, 212) 92%, rgb(29, 78, 192) 95%, rgb(25, 65, 165) 98%) repeat scroll 0% 0%;
 
-    &__start > img {
-        height: $height;
-        &:hover {
-            filter: brightness(105%);
+    &__start {
+        margin-right: 10px;
+        & > img {
+            height: $height;
+            &:hover {
+                filter: brightness(105%);
+            }
         }
     }
 
     &__tray {
         background: rgba(0, 0, 0, 0) linear-gradient(rgb(12, 89, 185) 1%, rgb(19, 158, 233) 6%, rgb(24, 181, 242) 10%, rgb(19, 155, 235) 14%, rgb(18, 144, 232) 19%, rgb(13, 141, 234) 63%, rgb(13, 159, 241) 81%, rgb(15, 158, 237) 88%, rgb(17, 155, 233) 91%, rgb(19, 146, 226) 94%, rgb(19, 126, 215) 97%, rgb(9, 91, 201) 100%) repeat scroll 0% 0%;
         border-left: 1px solid rgb(16, 66, 175);
+        font-size: 11px;
     }
 }
 </style>
